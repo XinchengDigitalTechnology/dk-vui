ec49f7995605fc05027102ab8caffc5d1fddbac3
blryli <405549416@qq.com>
2023-08-14 18:39:04 +0800

    permission

--- packages/Button/src/index.vue
+++ packages/Button/src/index.vue
@@ -1,10 +1,12 @@
 <template>
-  <el-button v-if="$permission(auth)" v-bind="$attrs"><slot/></el-button>
+  <el-button v-if="permission(auth)" v-bind="$attrs"><slot/></el-button>
 </template>
 
 <script setup name="VButton">
 const props = defineProps({
   auth: { type: String, default: '' },
 })
-const $permission = inject('$permission') || (() => true)
+const permission = (val) => {
+  return (window.BTN_AUTH || []).includes(val)
+}
 </script>
