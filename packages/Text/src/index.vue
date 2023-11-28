46955e76c45ed2e5a587c7d825dbad3c93ba8c3e
blryli <405549416@qq.com>
2023-11-28 10:09:11 +0800

    文本组件默认值处理

--- packages/Text/src/index.vue
+++ packages/Text/src/index.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="v-text">
     <span v-if="title" class="v-text-title">{{ title }}:</span>
-    <div v-if="line === 'full'" class="v-text-content">{{ showValue }}</div>
+    <div v-if="nothing">-</div>
     <template v-else>
       <div class="v-text-content-wrapper" :style="{ maxHeight: lineNum * 24 + 'px' }" @mouseenter="mouseenter"
         @mouseleave="mouseleave">
         <slot>
-          <div ref="valueRef" class="v-text-content" :class="`is--${type} ${disabled ? 'is--disabled' : ''}`" :style="style" @click="!disabled && type && emit('click')">{{ showValue }}</div>
-          <div ref="textRef" class="v-text-content-wrap">{{ showValue }}</div>
+          <div ref="valueRef" class="v-text-content" :class="`is--${type} ${disabled ? 'is--disabled' : ''}`"
+            :style="style" @click="!disabled && type && emit('click')">{{ value }}</div>
+          <div ref="textRef" class="v-text-content-wrap">{{ value }}</div>
         </slot>
         <!-- 未溢出时的复制 -->
         <div v-if="copy && !isOverflow" class="v-text-btns" title="复制" @click.stop="copyText">
@@ -30,6 +31,7 @@
 import { $copyToClipboard } from "@xqsit94/vue3-copy-to-clipboard"
 import { ElMessage } from "element-plus"
 import { DocumentCopy } from "@element-plus/icons-vue"
+import { useSlots } from "vue";
 
 const props = defineProps({
   value: { type: [Number, String], default: '' }, // 文本
@@ -42,9 +44,17 @@ const props = defineProps({
 
 const emit = defineEmits(['click'])
 
+const slots = useSlots()
+const nothing = ref(false)
+const { children } = slots.default && slots.default()[0] || {}
+if(slots.default && !children || !props.value && props.value !== 0) {
+  nothing.value = true
+}
+
+
 // 溢出
 const lineNum = +props.line
-const style = props.line === 'full' ? {} : lineNum > 1 ? {
+const style = lineNum > 1 ? {
   'display': '-webkit-box',
   '-webkit-box-orient': 'vertical',
   '-webkit-line-clamp': lineNum
@@ -53,17 +63,10 @@ const style = props.line === 'full' ? {} : lineNum > 1 ? {
   'white-space': 'nowrap'
 }
 
-// 显示的值/默认值
-const showValue = computed(() => {
-  const { value } = props
-  if (value === 0) return 0
-  return props.value || '-'
-})
-
 // 复制
 const copyText = () => {
   try {
-    $copyToClipboard(showValue.value)
+    $copyToClipboard(props.value)
     ElMessage.success("复制成功")
   } catch (error) {
     ElMessage.error(error || "复制失败")
@@ -77,14 +80,14 @@ const textRef = ref()
 const isOverflow = ref(false)
 
 const mouseenter = ({ target }) => {
-  const {offsetWidth, offsetHeight} = textRef?.value || {}
-  const {offsetWidth: valueOffsetWidth} = valueRef?.value || {}
-  if(!offsetWidth || !offsetHeight || !valueOffsetWidth) return
+  const { offsetWidth, offsetHeight } = textRef?.value || {}
+  const { offsetWidth: valueOffsetWidth } = valueRef?.value || {}
+  if (!offsetWidth || !offsetHeight || !valueOffsetWidth) return
   isOverflow.value = offsetHeight / 24 > lineNum || valueOffsetWidth < offsetWidth
   if (!isOverflow.value) return
   updateTip && updateTip({
     visible: true,
-    content: showValue.value,
+    content: props.value,
     ref: target
   })
 }
@@ -100,7 +103,7 @@ const mouseleave = () => {
 </script>
 
 <style lang="scss">
-:root{
+:root {
   --v-text--title-color: #666;
   --v-text--content-color: #333;
   --v-text--line-height: 1.4rem;
@@ -148,18 +151,20 @@ const mouseleave = () => {
     &:hover {
       color: var(--el-color-primary-light-3);
     }
-    &.is--disabled{
+
+    &.is--disabled {
       opacity: .6;
       cursor: no-drop;
     }
   }
 
-  .is--button{
+  .is--button {
     user-select: none;
   }
 
   .is--link {
     position: relative;
+
     &:not(.is--disabled):hover {
       &::after {
         content: "";
@@ -190,5 +195,4 @@ const mouseleave = () => {
       transform: translateX(0) scale(1);
     }
   }
-}
-</style>
+}</style>
