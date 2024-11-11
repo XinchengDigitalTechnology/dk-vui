d090ff24e0c04d5e64fc5199f2e13e3b6a29383d
AshinChen <903048273@qq.com>
2024-11-11 16:26:35 +0800

    icon

--- packages/components/Select/Paste.vue
+++ packages/components/Select/Paste.vue
@@ -2,9 +2,10 @@
   <el-popover v-model:visible="visible" trigger="click" placement="bottom-end" :offset="2" :show-arrow="false" width="405px" @show="open" @hide="handleClose">
     <template #reference>
       <el-button>
-        <el-icon>
+        <!-- <el-icon>
           <Tickets />
-        </el-icon>
+        </el-icon> -->
+        <svg t="1731313431916" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3728" width="16" height="16"><path d="M376.512 351.36h103.36a32 32 0 1 1 0 64H376.512a32 32 0 0 1 0-64zM376.512 499.328h270.72a32 32 0 1 1 0 64h-270.72a32 32 0 0 1 0-64zM376.512 647.296h270.72a32 32 0 0 1 0 64h-270.72a32 32 0 1 1 0-64z" p-id="3729"></path><path d="M607.552 145.92l195.2 195.136v482.112a54.912 54.912 0 0 1-54.976 54.912H276.224a54.912 54.912 0 0 1-54.912-54.912V200.832c0-30.336 24.576-54.912 54.912-54.912h331.328z m-26.496 64H285.248v604.096h453.44V367.552L581.056 209.92z" p-id="3730"></path></svg>
       </el-button>
     </template>
     <div class="v-select-paste">
