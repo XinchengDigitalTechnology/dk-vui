059c998a5c33cbd7e4438f6fabf8d544506505d7
blryli <405549416@qq.com>
2025-11-18 17:10:32 +0800

    fix autoload

--- packages/components/Table/index.vue
+++ packages/components/Table/index.vue
@@ -363,16 +363,13 @@ onActivated(() => {
   }, 100)
 })
 
-watch(
-  () => isTableContentLoad.value,
-  () => {
-    nextTick(async() => {
-      // 表格加载完毕后，没有加载数据，则主动请求一次
-      if (!loadData.value && merge.autoLoadQuery) await query()
-      columnList.value = gridRef?.value?.getColumns()
-    })
-  }
-)
+const tableLoad = () => {
+  nextTick(async() => {
+    // 表格加载完毕后，没有加载数据，则主动请求一次
+    if (!loadData.value && merge.autoLoadQuery) await query()
+    columnList.value = gridRef?.value?.getColumns()
+  })
+}
 
 // 处理固定列
 const columnList = ref(merge.columns)
@@ -540,7 +537,7 @@ defineExpose({ getForm, setForm, setFormField, resetForm, query, initColumn, get
       </div>
     </div>
     <div ref="contentRef" class="vx-table__content" v-dom-load="contentLoad">
-      <vxe-grid v-if="isTableContentLoad" ref="gridRef" v-bind="attrs" :height="tableHeight" :cell-style="cellStyle" :header-cell-style="cellStyle"
+      <vxe-grid v-if="isTableContentLoad"  v-dom-load="tableLoad" ref="gridRef" v-bind="attrs" :height="tableHeight" :cell-style="cellStyle" :header-cell-style="cellStyle"
         :header-cell-class-name="cellClassName" :cell-class-name="cellClassName" @scroll="handleScroll" @resizable-change="resizableChange" @sortChange="sort"
         @checkbox-change="checkboxChange" @checkbox-all="checkboxAll">
         <template v-for="name in slots.filter(d => !['form', 'high_form'].includes(d))" #[name]="row">
