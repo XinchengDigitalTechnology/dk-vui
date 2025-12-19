7475fdc0dafeca6dee6690beb0761f3ab66fa913
AshinChen <903048273@qq.com>
2025-12-19 13:42:53 +0800

    BatchInput

--- packages/components/BatchInput/index.vue
+++ packages/components/BatchInput/index.vue
@@ -1,7 +1,7 @@
 <template>
   <el-popover v-model:visible="visible" :disabled="disabled" trigger="click" placement="bottom-end" width="405px" :offset="2" :show-arrow="false" @show="open" @hide="close">
     <template #reference>
-      <el-input v-model="showValue" :placeholder="$attrs.placeholder || '请输入'" :class="['v-batch-input', showValue && !visible && 'is--value']" v-bind="$attrs">
+      <el-input v-model="showValue" :placeholder="$attrs.placeholder || '请输入'" :class="['v-batch-input', showValue && !visible && 'is--value']" v-bind="$attrs" ref="elInputRef">
         <template #suffix>
           <svg class="v-batch-input-icon" width="14px" height="14.00px" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
             <path fill="#909399"
@@ -18,7 +18,7 @@
               d="M670.72 449.097143a192.365714 192.365714 0 1 0 191.634286 192.365714 192.365714 192.365714 0 0 0-191.634286-192.365714z m0 320.365714a128 128 0 1 1 128-128 128 128 0 0 1-128 128z" />
           </svg>
        
-          <svg :class="['v-batch-input-clear', uniqueClass]" t="1731313372920" viewBox="0 0 1024 1024" version="1.1"
+          <svg class="v-batch-input-clear" t="1731313372920" viewBox="0 0 1024 1024" version="1.1"
             xmlns="http://www.w3.org/2000/svg" p-id="3649" width="19" height="19">
             <path d="M512 96a416 416 0 1 1 0 832 416 416 0 0 1 0-832z m0 64a352 352 0 1 0 0 704 352 352 0 0 0 0-704z" p-id="3650" fill="currentColor"></path>
             <path d="M365.12 387.328a32 32 0 0 1 44.864-45.248l4.096 4.032 244.8 290.56a32 32 0 0 1-44.864 45.248l-4.096-4.032-244.8-290.56z" p-id="3651" fill="currentColor"></path>
@@ -47,10 +47,7 @@ const emit = defineEmits(["update:modelValue", "search"])
 const visible = ref(false)
 const disabled = ref(false)
 const inputRef = ref()
-
-// 生成唯一ID用于区分多个组件实例
-const uniqueId = `batch-input-${Math.random().toString(36).substr(2, 9)}`
-const uniqueClass = `v-batch-input-clear-${uniqueId}`
+const elInputRef = ref()
 
 const inputValue = computed({
   get() {
@@ -98,7 +95,7 @@ const handleMouseLeave = () => {
 }
 
 onMounted(() => {
-  clearIcon = document.querySelector(`.${uniqueClass}`)
+  clearIcon = elInputRef.value?.$el?.querySelector('.v-batch-input-clear')
   if (clearIcon) {
     clearIcon.addEventListener('click', clear)
     clearIcon.addEventListener('mouseenter', handleMouseEnter)
